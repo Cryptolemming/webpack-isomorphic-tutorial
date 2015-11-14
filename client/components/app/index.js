1809077ef5e7a9791b6c65591ec72742770e37c4
cryptolemming <aliayoub22@gmail.com>
2015-11-14 11:16:57 -0500

    first commit after boilerplate

---
+++ client/components/app/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {} from './style.less';
+
+class App extends Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return <div>Hello from React Component</div>;
+	}
+}
+
+export default App;
