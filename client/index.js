e3866397778f9f12e6a61a507872ed017b10ed6d
cryptolemming <aliayoub22@gmail.com>
2015-11-14 16:40:40 -0500

    create intiial Store client-side in client/index.js same way as server

--- client/index.js
+++ client/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from 'components/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const initialState = window.INITIAL_STATE;
+const store = createStore((state=initialState) => state);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('app'));
