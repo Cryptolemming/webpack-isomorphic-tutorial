0aa2003eff9c3d9cf49eda64ad1846eaf993082b
cryptolemming <aliayoub22@gmail.com>
2015-11-14 17:31:57 -0500

    update client/index.js to use reducers to create the store

--- client/index.js
+++ client/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from 'components/app';
+import reducers from 'reducers';
 
 const initialState = window.INITIAL_STATE;
-const store = createStore((state=initialState) => state);
+const store = createStore(reducers(initialState));
 
 ReactDOM.render(
 	<Provider store={store}>
